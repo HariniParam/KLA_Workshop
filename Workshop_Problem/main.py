b809bf91cce3eb6b1ec0ca2b19f1f16507f202b2
Harini P <102685359+HariniParam@users.noreply.github.com>
2023-06-23 16:34:53 +0530

    Final code

--- Workshop_Problem/main.py
+++ Workshop_Problem/main.py
@@ -10,6 +10,7 @@ import os
 import json
 import csv
 
+defect=[]
 #Reading data from json file
 def read_json(file_path):
     with open(file_path , 'r') as json_file:
@@ -32,47 +33,137 @@ def image_details(data):
     bottom_right_x = bottom_right["x"]
     bottom_right_y = bottom_right["y"]
 
+#Adding image path into list
 def image_array(image_list, row, columns):
     for i in range(row):
         for j in range(columns):
             image_path = os.getcwd() + "\Level_1_Input_Data\wafer_image_" + str((j+1)) + ".png"
             image_list.append(image_path)
     print(image_list)
-           
+       
+#Detecting defect    
 def image_defects(image_list):
-    """care_area_width = bottom_right_x - top_left_x
-    care_area_height = top_left_y - bottom_right_y
-    image = Image.new("RGB",(care_area_width, care_area_height))
-    i = Image.open(image_path)
-    i.show()
-    for i in range(care_area_height):
-        for j in range(care_area_width):"""
+    """with open("output.csv","w") as output_file:
+        csv_write = csv.writer(output_file)
+        
+        for i in range(len(image_list)):
+            image = Image.open(image_list[i])
+            rgb_image1 = image.convert('RGB')
+            wid1, hgt1 = rgb_image1.size
+            for y in range(hgt1):
+                for x in range(wid1):
+                    r, g, b = rgb_image1.getpixel((x, y))
+                    color1 = (r,g,b)
+                    image = Image.open(image_list[i+1])
+                    rgb_image = image.convert('RGB')
+                    r1, g1, b1 = rgb_image.getpixel((x, y))
+                    color = (r1,g1,b1)
+                    if color1 != color:    
+                        #print(defect)
+                        defect.append((i+1,x,total_height-1-y))
+                        #csv_write.writerow((i+1,x,total_height-1-y))
+        print(defect)             
+        csv_write.writerows(defect)"""
+        
     
-
+        
     with open("output.csv","w") as output_file:
         csv_write = csv.writer(output_file)
+        image = Image.open(image_list[0])
+        rgb_image1 = image.convert('RGB')
+        wid1, hgt1 = rgb_image1.size
+        image2 = Image.open(image_list[1])
+        rgb_image2 = image2.convert('RGB')
         defect = []
-        for i in range(len(image_list)):
-            for j in range(len(image_list)):
-                image = Image.open(image_list[i])
-                rgb_image1 = image.convert('RGB')
-                wid1, hgt1 = rgb_image1.size
-                for y in range(hgt1):
-                    for x in range(wid1):
-                        r, g, b = rgb_image1.getpixel((x, y))
-                        color1 = (r,g,b)
-                        if i!=j:
-                            image = Image.open(image_list[j])
-                            rgb_image = image.convert('RGB')
-                            r1, g1, b1 = rgb_image.getpixel((x, y))
-                            color = (r1,g1,b1)
-                            if color1 != color:    
-                                if (i+1,x,y) not in defect:
-                                    defect.append((i+1,x,y))
-                                    print(defect)
-                                    csv_write.writerow((i+1,x,y))
-        #print(defect)
-            
+        for y in range(hgt1):
+            for x in range(wid1):
+                r, g, b = rgb_image1.getpixel((x, y))
+                color1 = (r,g,b)
+                r1, g1, b1 = rgb_image2.getpixel((x, y))
+                color2 = (r1,g1,b1)
+                if color1 != color2:
+                    #print(color1, color2)
+                    #if (1,x,y) not in defect:
+                    defect.append((1,x,total_height-1-y))
+        print(defect)
+        csv_write.writerows(defect)
+        
+        image = Image.open(image_list[1])
+        rgb_image1 = image.convert('RGB')
+        wid1, hgt1 = rgb_image1.size
+        image2 = Image.open(image_list[2])
+        rgb_image2 = image2.convert('RGB')
+        defect = []
+        for y in range(hgt1):
+            for x in range(wid1):
+                r, g, b = rgb_image1.getpixel((x, y))
+                color1 = (r,g,b)
+                r1, g1, b1 = rgb_image2.getpixel((x, y))
+                color2 = (r1,g1,b1)
+                if color1 != color2:
+                    #print(color1, color2)
+                        #if (1,x,y) not in defect:
+                    defect.append((2,x,total_height-1-y))
+        print(defect)
+        csv_write.writerows(defect)
+        
+        image = Image.open(image_list[2])
+        rgb_image1 = image.convert('RGB')
+        wid1, hgt1 = rgb_image1.size
+        image2 = Image.open(image_list[3])
+        rgb_image2 = image2.convert('RGB')
+        defect = []
+        for y in range(hgt1):
+            for x in range(wid1):
+                r, g, b = rgb_image1.getpixel((x, y))
+                color1 = (r,g,b)
+                r1, g1, b1 = rgb_image2.getpixel((x, y))
+                color2 = (r1,g1,b1)
+                if color1 != color2:
+                    #print(color1, color2)
+                        #if (1,x,y) not in defect:
+                    defect.append((3,x,total_height-1-y))
+        print(defect)
+        csv_write.writerows(defect)
+        
+        image = Image.open(image_list[3])
+        rgb_image1 = image.convert('RGB')
+        wid1, hgt1 = rgb_image1.size
+        image2 = Image.open(image_list[4])
+        rgb_image2 = image2.convert('RGB')
+        defect = []
+        for y in range(hgt1):
+            for x in range(wid1):
+                r, g, b = rgb_image1.getpixel((x, y))
+                color1 = (r,g,b)
+                r1, g1, b1 = rgb_image2.getpixel((x, y))
+                color2 = (r1,g1,b1)
+                if color1 != color2:
+                    #print(color1, color2)
+                        #if (1,x,y) not in defect:
+                    defect.append((4,x,total_height-1-y))
+        print(defect)
+        csv_write.writerows(defect)
+        
+        image = Image.open(image_list[4])
+        rgb_image1 = image.convert('RGB')
+        wid1, hgt1 = rgb_image1.size
+        image2 = Image.open(image_list[0])
+        rgb_image2 = image2.convert('RGB')
+        defect = []
+        for y in range(hgt1):
+            for x in range(wid1):
+                r, g, b = rgb_image1.getpixel((x, y))
+                color1 = (r,g,b)
+                r1, g1, b1 = rgb_image2.getpixel((x, y))
+                color2 = (r1,g1,b1)
+                if color1 != color2:
+                    #print(color1, color2)
+                        #if (1,x,y) not in defect:
+                    defect.append((5,x,total_height-1-y))
+        print(defect)
+        csv_write.writerows(defect)
+        
 
 path = os.getcwd()
 json_path = path+"\Level_1_Input_Data\input.json"
@@ -85,7 +176,6 @@ image_list = []
 image_array(image_list, row, columns)
 
 image_defects(image_list)
-
         
 
 
